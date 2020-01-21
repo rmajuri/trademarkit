d3c739021da18126e7a2578e7c14457bece0e2d8
Rob Majuri <robmajuri@gmail.com>
2020-01-20 22:13:16 -0600

    Try end point

--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,23 @@ const Home = () => {
   const [trademarks, setTrademarks] = useState([]);
   const [searchPhrase, setSearchPhrase] = useState('');
 
+  const handleSearchSubmit = event => {
+    event.preventDefault()
+    fetch(`https://markerapi.com/api/v2/trademarks/trademark/${encodeURI(searchPhrase)}/status/all/start/1/username/merman/password/7xRdLjM2Bh`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  };
+
   return (
     <div className="Home">
       <header className="Home-header">
         <h1>trademarkit</h1>
       </header>
-      <Search searchPhrase={searchPhrase} setSearchPhrase={setSearchPhrase}  />
+      <Search
+      searchPhrase={searchPhrase}
+      setSearchPhrase={setSearchPhrase}
+      handleSearchSubmit={handleSearchSubmit} />
       <ResultsTable trademarks={trademarks} />
     </div>
   );
