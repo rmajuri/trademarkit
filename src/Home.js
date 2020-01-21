c6a9832524fca87785714d597449524510e0b883
Rob Majuri <robmajuri@gmail.com>
2020-01-20 21:04:25 -0600

    Start handling state

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,15 @@ import Search from './search/search'
 import ResultsTable from './results-table/results-table'
 
 const Home = () => {
+  const [trademarks, setTrademarks] = useState([]);
+  const [searchPhrase, setSearchPhrase] = useState('');
 
   return (
     <div className="Home">
       <header className="Home-header">
         <h1>trademarkit</h1>
       </header>
-      <Search />
+      <Search searchPhrase={searchPhrase} setSearchPhrase={setSearchPhrase}  />
       <ResultsTable trademarks={trademarks} />
     </div>
   );
