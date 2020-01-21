91b38c977acf7688ee99ab9e566e00b5617d1bc5
Rob Majuri <robmajuri@gmail.com>
2020-01-20 22:18:50 -0600

    Bind search state to search component

--- src/search/search.js
+++ src/search/search.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { TextField } from '@material-ui/core'
 import styles from './search.module.css'
 
-const Search = () => (
+//Pass state and event handlers from the Home component to child search component
+const Search = ({ searchPhrase, setSearchPhrase, handleSearchSubmit}) => (
     <div className={styles.Container}>
-        <form>
+        <form onSubmit={handleSearchSubmit}>
             <TextField
                 className={styles.ContainerInput}
                 label="Search trademarks"
                 variant="outlined"
-                placeholder="Enter a brand name idea to see relevant trademark information" />
+                placeholder="Enter a brand name idea to see relevant trademark information"
+                //Update the search phrase state on every user key stroke
+                onChange={event => setSearchPhrase(event.target.value)}
+                //Bind the search phrase state to the search input
+                value={searchPhrase}
+                />
         </form>
     </div>
 );
