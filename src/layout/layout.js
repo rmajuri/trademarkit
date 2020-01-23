b74c84d9390b089cea83ee7df4d0878895125093
Rob Majuri <robmajuri@gmail.com>
2020-01-22 21:47:15 -0600

    Add layout component

---
+++ src/layout/layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './layout.module.css'
+
+const Layout = props => (
+    <>
+        <header className={styles.Header}>
+            <h1>trademarket</h1>
+        </header>
+            {props.children}
+        <footer className={styles.Footer}>Made with &#8482; by <a href="https://www.robertmajuri.com/" target="blank">Rob Majuri</a></footer>
+    </>
+);
+
+export default Layout;
