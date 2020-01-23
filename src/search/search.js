40b4840af1d0f9e433b223146f34c3a0ea005797
Rob Majuri <robmajuri@gmail.com>
2020-01-22 20:17:17 -0600

    Style button through CSS instead of JSS

--- src/search/search.js
+++ src/search/search.js
@@ -1,21 +1,9 @@
-import React from 'react'
-import { TextField, Button } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import styles from './search.module.css'
-
-//JSS is the most convenient way to add custom styling to Material UI components
-//This will be used to change the positioning of the MUI button component
-const useStyles = makeStyles({
-    root: {
-        marginTop: '.5em',
-        alignSelf: 'flex-end'
-    }
-});
+import React from 'react';
+import { TextField, Button } from '@material-ui/core';
+import styles from './search.module.css';
 
 //Pass state and event handlers from the Home component to child search component
 const Search = ({ searchPhrase, setSearchPhrase, setAreResultsEmpty, handleSearchSubmit}) => {
-
-    const classes = useStyles();
 
     const handleSearchPhraseChange = event => {
         setSearchPhrase(event.target.value);
@@ -38,7 +26,7 @@ const Search = ({ searchPhrase, setSearchPhrase, setAreResultsEmpty, handleSearc
                         value={searchPhrase}
                         required
                         />
-                    <Button type="submit" label="submit" className={classes.root}>submit</Button>
+                    <Button type="submit" label="submit" className={styles.FormButton}>submit</Button>
                 </form>
             </div>);
 };
