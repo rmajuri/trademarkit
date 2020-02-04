4b81894204e171957b9999b01c6413adfbe0412a
Rob Majuri <robmajuri@gmail.com>
2020-02-03 20:12:05 -0600

    Add NoSSR component temporarily while solving style render issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
+import { NoSsr } from '@material-ui/core'
 import Switch from 'react-router-dom/Switch';
 import Home from './Home';
 import './App.css';
 
+
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-  </Switch>
+  <NoSsr>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </NoSsr>
 );
 
 export default App;
