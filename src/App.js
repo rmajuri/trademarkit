a83093340e5eb7c40bdf6c6282edcf01f6c768fb
Rob Majuri <robmajuri@gmail.com>
2020-02-03 20:48:49 -0600

    Syntax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Route from 'react-router-dom/Route';
-import { NoSsr } from '@material-ui/core'
+import { NoSsr } from '@material-ui/core';
 import Switch from 'react-router-dom/Switch';
 import Home from './Home';
 import './App.css';
