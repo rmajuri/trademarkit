b1df4430db9363cef0e8999a5c3d2992bfe4aba5
Rob Majuri <rob.majuri@civicsource.com>
2020-02-05 08:48:49 -0600

    Remove commented code again

--- src/placeholder/placeholder.js
+++ src/placeholder/placeholder.js
@@ -3,21 +3,6 @@ import Proptypes from 'prop-types';
 import { Typography } from '@material-ui/core';
 import styles from './placeholder.module.css';
 
-
-// const Placeholder = ({areResultsEmpty, searchPhrase}) => ( areResultsEmpty && searchPhrase && searchPhrase.length  ?
-//     (<div className={styles.PlaceHolderEmptyResults}>
-//         <Typography variant="h5">
-//             Sorry - we've got nothin'.
-//         </Typography>
-//      </div>
-//     ) :
-//     (
-//         <div className={styles.PlaceholderTM}>
-//             <span>&#8482;</span>
-//         </div>
-//     )
-// );
-
 const Placeholder = ({areResultsEmpty, searchPhrase}) => ( 
         <div className={styles.Placeholder}>
             {areResultsEmpty && searchPhrase && searchPhrase.length  ?
