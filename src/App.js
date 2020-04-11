4913b4a84fbaabe713d76b799f6dad54e39b441f
Rob Majuri <robmajuri@gmail.com>
2020-04-11 16:21:04 -0400

    Response to popstate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Route, { useEffect } from 'react-router-dom/Route';
+import React, { useEffect } from 'react';
+import Route from 'react-router-dom/Route';
 import { NoSsr } from '@material-ui/core';
 import Switch from 'react-router-dom/Switch';
 import Home from './Home';
@@ -8,8 +8,10 @@ import './App.css';
 const App = () => {
 
   useEffect(() => {
-    window.onpopstate = () => window.location.reload();
-  });
+    window.addEventListener('popstate', function() {
+      window.location.reload();
+    });
+  }, []);
 
   return (
     <NoSsr>
